99bf1d88d094b0e50bf573315cb43ad9c1253c17
lvl12sealclubber <maxpower713@gmail.com>
2018-07-09 14:11:45 -0400

    First Angular Update

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, style} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: `<h2>Hello World </h2>`,
+    style: `
+    h2{
+        font-size:22px;
+        font-family:sanserif;
+    }
+    `
+})
+export class AppComponent {
+
+}
